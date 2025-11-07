b1adc5a20e4bb8a0fcff3319a63a9f8fb7a2624f
JacekO98 <jacekostrowski981@gmail.com>
2025-11-07 18:29:17 +0100

    gotowy przycisk Delete

--- IMS.WebApp/obj/Debug/net8.0/IMS.WebApp.AssemblyInfo.cs
+++ IMS.WebApp/obj/Debug/net8.0/IMS.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a19a89631c3cbe38aaa2fa9adca8e3ea625d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d381261d6af59094ab48dc668e62c523ed4614")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
