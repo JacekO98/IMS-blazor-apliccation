8acc7bb4c7c6595dcb20a9e470cf13ade6ca340a
JacekO98 <jacekostrowski981@gmail.com>
2025-11-07 18:33:48 +0100

    a

--- IMS.Use_cases/obj/Debug/net8.0/IMS.UseCases.AssemblyInfo.cs
+++ IMS.Use_cases/obj/Debug/net8.0/IMS.UseCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60835ce2abe4525334f07d39b177bb82f617856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a19a89631c3cbe38aaa2fa9adca8e3ea625d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
